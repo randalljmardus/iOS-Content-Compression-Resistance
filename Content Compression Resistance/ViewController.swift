6ccbdfcd010f20ad4674e38ff55fe7d0c7dd30d5
Randall Mardus <randalljmardus@gmail.com>
2016-02-24 12:52:39 -0500

    initial UI setup

--- Content Compression Resistance/ViewController.swift
+++ Content Compression Resistance/ViewController.swift
@@ -13,6 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+    let button1 = UIButton()
+        button1.setTitle("Button1", forState: .Normal)
+        
+    let button2 = UIButton()
+        button2.setTitle("Some really, really, really long title", forState: .Normal)
+        
+    button1.backgroundColor = UIColor.redColor()
+    button2.backgroundColor = UIColor.blueColor()
+        
+    button1.translatesAutoresizingMaskIntoConstraints = false
+    button2.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(button1)
+        view.addSubview(button2)
+        
+        button1.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor).active = true
+        button1.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 20).active = true
+    
+        button2.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 20).active = true
+        button2.leadingAnchor.constraintEqualToAnchor(button1.trailingAnchor).active = true
+        button2.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor).active = true
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
